1edc8b555569107c3fe1cc30d9ba23c687ece6aa
pblinkprosper <pblinkprosper01@gmail.com>
2024-10-11 14:40:57 +0000

    Relocated the directory

--- 0x00-python_variable_annotations/0-add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Type-annoted function
-"""
-
-
-def add(num1: float, num2: float) -> float:
-    """function that adds two floats and returns their output"""
-    return (num1 + num2)
